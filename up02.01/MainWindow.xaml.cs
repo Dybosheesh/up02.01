a9a0feabdf5947fc329a8115e3dcfe5ec5673d83
Dybosheesh <glanets456@gmail.com>
2022-12-10 02:10:12 +0300

    Сделал

--- up02.01/MainWindow.xaml.cs
+++ up02.01/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OfficeOpenXml;
+using Newtonsoft.Json;
+using OfficeOpenXml.Style;
+
 
 namespace up02._01
 {
@@ -54,6 +61,49 @@ namespace up02._01
                 }
             }
             personalgrid.ItemsSource = PersonalList;
+        }
+
+        private void excel_Click(object sender, RoutedEventArgs e)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            using (ExcelPackage excel = new ExcelPackage())
+            {
+                ExcelWorksheet sheet = excel.Workbook.Worksheets.Add("Sheet 1");
+                sheet.Cells[1, 1].Value = "ID"; ;
+                sheet.Cells[1, 2].Value = "IDent";
+                sheet.Cells[1, 3].Value = "Имя";
+                sheet.Cells[1, 4].Value = "Фамилия";
+                sheet.Cells[1, 5].Value = "Отчество";
+                sheet.Cells[1, 6].Value = "Должность";
+                List<Personal> PersonalList = new List<Personal>();
+                using (var db = new AppContext())
+                {
+                    var query = from b in db.personals select b;
+                    foreach (var item in query)
+                    {
+                        PersonalList.Add(item);
+                    }
+                }
+                sheet.Cells[2, 1].LoadFromCollection(PersonalList);
+                string Path = "PersonalList.xlsx";
+                FileInfo file_excel = new FileInfo(Path);
+                excel.SaveAs(file_excel);
+            }
+        }
+
+        private void json_Click(object sender, RoutedEventArgs e)
+        {
+            List<Personal> PersonalList = new List<Personal>();
+            using (var db = new AppContext())
+            {
+                var query = from b in db.personals select b;
+                foreach (var item in query)
+                {
+                    PersonalList.Add(item);
+                }
+            }
+            var json_file = JsonConvert.SerializeObject(PersonalList);
+            File.WriteAllText("PersonalList.json", json_file.ToString());
         }
     }
 }
